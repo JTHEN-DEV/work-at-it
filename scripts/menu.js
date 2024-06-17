e2d308c60d54053622c2ed954ff52cbd01f56e53
Joel Then <joel.yh.then@gmail.com>
2024-06-17 10:39:46 +0800

    Started on menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,21 @@
 // Script file to handle menu
 
+const loginMenu = document.getElementById("login-menu");
+const settingsMenu = document.getElementById("settings-menu");
+const menuContainer = document.getElementById("menu-container");
+
 class Menu {
-    static show() {}
+    static currentMenu = null;
+
+    static showMenu(menu) {
+        Menu.currentMenu = menu;
+        menu.style.display = "block";
+        menuContainer.style.display = "flex";
+    }
+
+    static hideMenu() {
+        Menu.currentMenu.style.display = "none";
+        Menu.currentMenu = null;
+        menuContainer.style.display = "none";
+    }
 }

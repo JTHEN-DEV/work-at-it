a0e7509a84c7d2a633899da0c80f994811b22357
Joel Then <joel.yh.then@gmail.com>
2024-06-18 13:13:58 +0800

    Refactored Desk Array

--- src/components/game-components/DeskArray.tsx
+++ src/components/game-components/DeskArray.tsx
@@ -4,25 +4,25 @@ import Desk from "./Desk";
 type Props = {};
 
 const PROPERTIES = {
-    ROW_SIZE: 10,
+    ROW_SIZE: 30,
     ROW_GAP: 2,
     COL_GAP: 2,
-    CUBE_LENGTH: 1,
-    CUBE_WIDTH: 1,
+    DESK_LENGTH: 1,
+    DESK_WIDTH: 1,
 };
 
 const DeskArray = (props: Props) => {
-    const cubes = Array.apply(null, Array(100));
+    const desks = Array.apply(null, Array(1000));
     return (
         <>
-            {cubes.map((_, index) => {
+            {desks.map((_, index) => {
                 const x =
                     (index % PROPERTIES.ROW_SIZE) *
-                    (PROPERTIES.CUBE_WIDTH + PROPERTIES.COL_GAP);
+                    (PROPERTIES.DESK_WIDTH + PROPERTIES.COL_GAP);
                 const y = 0;
                 const z =
                     Math.floor(index / PROPERTIES.ROW_SIZE) *
-                    (PROPERTIES.CUBE_LENGTH + PROPERTIES.ROW_GAP);
+                    (PROPERTIES.DESK_LENGTH + PROPERTIES.ROW_GAP);
                 return <Desk position={[x, y, z]} />;
                 // return <Desk position={[index, , z]} />;
             })}
